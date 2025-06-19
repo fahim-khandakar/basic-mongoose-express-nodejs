79a6812e73485b02205720058da2d54be0205a95
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-06-20 03:52:32 +0600

    feat: Update borrow routes to use consistent endpoint and improve response structure

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -1,16 +1,25 @@
 import express, { Request, Response } from "express";
+import { Borrow } from "../models/borrow.model";
 
 export const borrowRoutes = express.Router();
 
-borrowRoutes.post("/create-borrow", async (req: Request, res: Response) => {
-  res.status(201).json({
+borrowRoutes.post("/", async (req: Request, res: Response) => {
+  const body = req.body;
+  const data = await Borrow.create(body);
+
+  res.status(200).json({
     success: true,
-    message: "Note created successfully",
+    message: "Book borrowed successfully",
+    data,
   });
 });
+
 borrowRoutes.get("/", async (req: Request, res: Response) => {
-  res.status(201).json({
+  const data = await Borrow.find();
+
+  res.status(200).json({
     success: true,
     message: "Borrow records fetched successfully",
+    data,
   });
 });
