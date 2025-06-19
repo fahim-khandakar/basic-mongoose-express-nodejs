22ee67c01cda781197534ba76aab487737178cc8
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-06-20 03:37:17 +0600

    feat: Update book routes and improve response messages

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application, Request, Response } from "express";
+import { booksRoutes } from "./app/controllers/books.controller";
+import { borrowRoutes } from "./app/controllers/borrow.controller";
 
 const app: Application = express();
 
 app.use(express.json());
 
-// app.use("/books");
-// app.use("/borrow");
+app.use("/books", booksRoutes);
+app.use("/borrow", borrowRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Book Store App");
--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -1,27 +1,48 @@
 import express, { Request, Response } from "express";
+import { Book } from "../models/books.model";
 
 export const booksRoutes = express.Router();
 
-booksRoutes.post("/create-book", async (req: Request, res: Response) => {
+booksRoutes.post("/", async (req: Request, res: Response) => {
   const body = req.body;
+  const data = await Book.create(body);
 
-  res.status(201).json({
+  res.status(200).json({
     success: true,
     message: "Book created successfully",
+    data,
   });
 });
+
 booksRoutes.get("/", async (req: Request, res: Response) => {
-  res.status(201).json({
+  const { filter, sort, limit, sortBy } = req.query;
+  console.log("Query Parameters:", { filter, sort, limit, sortBy });
+
+  const filterQuery: Record<string, any> = {};
+  if (filter) {
+    filterQuery.genre = filter;
+  }
+
+  const data = await Book.find(filterQuery)
+    .sort({ [sortBy as string]: sort === "asc" ? 1 : -1 })
+    .limit(Number(limit));
+
+  res.status(200).json({
     success: true,
-    message: "Books fetched successfully",
+    message: "Books retrieved successfully",
+    data,
   });
 });
+
 booksRoutes.get("/:bookId", async (req: Request, res: Response) => {
   const bookId = req.params.bookId;
 
+  const data = await Book.findById(bookId);
+
   res.status(201).json({
     success: true,
-    message: "Book fetched successfully",
+    message: "Book retrieved successfully",
+    data,
   });
 });
 booksRoutes.delete("/:bookId", async (req: Request, res: Response) => {
