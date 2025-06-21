fb592505ad8a67c9d923a0a85c4ce125b759b207
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-06-21 12:29:25 +0600

    feat: Add Zod validation schemas for book and borrow creation; enhance input validation

--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -1,10 +1,53 @@
 import express, { Request, Response } from "express";
 import { Book } from "../models/books.model";
+import { z } from "zod";
 
 export const booksRoutes = express.Router();
 
+const CreateUserZodSchema = z.object({
+  title: z.string({ required_error: "Title is required" }),
+  author: z.string({ required_error: "Author is required" }),
+  genre: z.enum(
+    ["FICTION", "NON_FICTION", "SCIENCE", "HISTORY", "BIOGRAPHY", "FANTASY"],
+    { required_error: "Genre is required" }
+  ),
+  isbn: z.string({ required_error: "ISBN is required" }),
+  description: z.string().optional(),
+  copies: z.number({ required_error: "Copies is required" }).min(0, {
+    message: "Copies must be a positive number",
+  }),
+  available: z.boolean().optional(),
+});
+
+const UpdateUserZodSchema = z.object({
+  title: z.string().optional(),
+  author: z.string().optional(),
+  genre: z
+    .enum([
+      "FICTION",
+      "NON_FICTION",
+      "SCIENCE",
+      "HISTORY",
+      "BIOGRAPHY",
+      "FANTASY",
+    ])
+    .optional(),
+  isbn: z.string().optional(),
+  description: z.string().optional(),
+  copies: z
+    .number()
+    .min(0, {
+      message: "Copies must be a positive number",
+    })
+    .optional(),
+  available: z.boolean().optional(),
+});
+
 booksRoutes.post("/", async (req: Request, res: Response) => {
   const body = req.body;
+
+  CreateUserZodSchema.safeParse(body);
+
   const data = await Book.create(body);
 
   res.status(200).json({
@@ -61,6 +104,8 @@ booksRoutes.patch("/:bookId", async (req: Request, res: Response) => {
   const bookId = req.params.bookId;
   const updatedBody = req.body;
 
+  UpdateUserZodSchema.safeParse(updatedBody);
+
   const existingBook = await Book.findById(bookId);
   if (!existingBook) {
     res.status(404).json({
@@ -72,12 +117,12 @@ booksRoutes.patch("/:bookId", async (req: Request, res: Response) => {
 
   if (
     (existingBook.available === false && existingBook.copies === 0) ||
-    updatedBody.available === true
+    updatedBody.data.available === true
   ) {
-    updatedBody.available = true;
+    updatedBody.data.available = true;
   }
 
-  const data = await Book.findByIdAndUpdate(bookId, updatedBody, {
+  const data = await Book.findByIdAndUpdate(bookId, updatedBody.data, {
     new: true,
   });
 
